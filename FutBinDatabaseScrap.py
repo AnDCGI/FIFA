6d400d4b86d15282a2f4ea6eeec3a99a34322f0d
AnDCGI <rootandcgi@gmail.com>
2022-02-08 17:55:23 +0530

    Everything Fixed
    Exception are happening
    line 82, in <module>
        bodyType = re.search(r'\s{6}(\w.+?)\s{2}', cardDetails).group(1)
    AttributeError: 'NoneType' object has no attribute 'group'

--- FutBinDatabaseScrap.py
+++ FutBinDatabaseScrap.py
@@ -9,8 +9,8 @@ fifa = {'22': 'FIFA22'}     # Store Key
 cardColumns = ['ID', 'Name', 'Rating', 'Position', 'Revision', 'Nation',
                'Club', 'League', 'Price | PS', 'WeakFoot', 'Skill Moves',
                'Pace', 'Shooting', 'Passing', 'Dribbling', 'Defending',
-               'Phyiscality', 'Body Type', 'Weight', 'Popularity', 'BaseStats',
-               'InGameStats', 'WorkRate', 'Height']
+               'Phyiscality', 'Body Type', 'Weight', 'Height', 'WorkRate',
+               'Popularity', 'BaseStats', 'InGameStats']
 
 C = open('FutBin_Players_Stats_FIFA_22_FUT.csv', 'w')
 C.write(','.join(cardColumns) + '\n')
@@ -33,7 +33,7 @@ for key, value in fifa.items():
             'li', {'class': 'page-item'})[-2].text).strip()
     print('Number of pages to be parsed for FIFA '
           + key + ' is ' + TotalPages + ' Pages')
-    for page in range(1, 2):  # int(TotalPages) + 1):
+    for page in range(1, int(TotalPages) + 1):
         FutBin = scraper.get('https://www.futbin.com/'
                              + key + '/players?page=' + str(page))
         bs = bs4.BeautifulSoup(FutBin.text, 'html.parser')
@@ -59,32 +59,45 @@ for key, value in fifa.items():
             # Getting Work Rate W/R
             workRate = re.search('\\w\\\\\\w', cardDetails,
                                  re.IGNORECASE).group(0)
-            # Removing W/R From cardDetails
+            # Removing workRate From cardDetails
             cardDetails = re.sub('\\w\\\\\\w', '', cardDetails)
             # Getting Height CM|Feet'Inch"
-            match_Height = re.search(
+            matchHeight = re.search(
                 '\\w+\\|\\d\'\\d+\"', cardDetails, re.IGNORECASE).group(0)
-            # Removing Height CM|Feet'Inch" From cardDetails
+            # Removing matchHeight From cardDetails
             cardDetails = re.sub('\\w+\\|\\d\'\\d+\"', '', cardDetails)
-            # print(cardDetails)
             # Getting Player Preffered Position
             position = re.findall('\\s(\\D*\\s\\D+)',
                                   cardDetails, re.IGNORECASE)[1].split()[0]
+            # Removing position From cardDetails
             cardDetails = re.sub(str(position), '', cardDetails)
+            # Getting Player Card Revision
             revision = re.findall(
                 '\\s(\\D*\\s\\D+)', cardDetails, re.IGNORECASE)[1].split()
+            # Some revision Have Differnt Dimention, Joining Those
+            revision = (' '.join(revision))     # Now a String
+            # Removing revision From cardDetails
+            cardDetails = re.sub(revision, '', cardDetails)
+            # Getting Body Type
+            bodyType = re.search(r'\s{6}(\w.+?)\s{2}', cardDetails).group(1)
+            # Removing bodyType From cardDetails
+            cardDetails = re.sub(bodyType, '', cardDetails)
             cardDetails = cardDetails.split()
-
+            # Insert Tables At Proper ID
             cardDetails.insert(0, id)
             cardDetails.insert(1, name)
             cardDetails.insert(3, position)
+            cardDetails.insert(4, revision)
             cardDetails.insert(5, nation)
             cardDetails.insert(6, club)
             cardDetails.insert(7, league)
-            cardDetails.extend([workRate, match_Height])
+            cardDetails.insert(17, bodyType)
+            cardDetails.insert(19, matchHeight)
+            cardDetails.insert(20, workRate)
             Card.append(cardDetails)
             print(cardDetails)
             id += 1
+
         df = pd.DataFrame(Card)
-        df.to_csv('FutBin_Players_Stats_FIFA_22_FUT.csv', mode='a', header=False,
-                  sep=',', encoding='latin1', index=False)
+        df.to_csv('FutBin_Players_Stats_FIFA_22_FUT.csv', mode='a',
+                  header=False, sep=',', encoding='latin1', index=False)
